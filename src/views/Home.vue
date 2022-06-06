c64a799bf1853a8d2fd152b323ce9f532df51405
Enri <ibenrique2510@gmail.com>
2022-06-06 17:05:55 +0700

    Implement radial progress bar for exam page (#7)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,60 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import RadialProgressBar from 'vue3-radial-progress'
+
+const totalQuestions = ref(40)
+const answeredQuestions = ref(0)
+
+function answerQuestion() {
+  if (answeredQuestions.value < totalQuestions.value) {
+    answeredQuestions.value++
+  }
+}
+function deselectQuestion() {
+  if (answeredQuestions.value > 0) {
+    answeredQuestions.value--
+  }
+}
+</script>
+
 <template>
   <div class="root">
-    <h1>Home page</h1>
+    <button @click="deselectQuestion">-</button>
+    <radial-progress-bar
+      :diameter="200"
+      :completed-steps="answeredQuestions"
+      :total-steps="totalQuestions"
+      :animateSpeed="300"
+      start-color="#FFA800"
+      stop-color="#D68600"
+    >
+      <span class="progress">
+        {{ `${answeredQuestions} / ${totalQuestions}` }}
+      </span>
+    </radial-progress-bar>
+    <button @click="answerQuestion">+</button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .root {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style lang="scss" scoped>
+.root {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  .progress {
+    font-size: 20px;
+    color: var(--color-white);
+  }
+
+  button {
+    color: var(--color-white);
+    font-size: 20px;
+    border-radius: 5px;
+    padding: 10px 20px;
+    margin: 0 20px;
+    background-color: var(--color-primary-soft);
   }
 }
 </style>
